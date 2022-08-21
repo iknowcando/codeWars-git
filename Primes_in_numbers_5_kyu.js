c52d939d14f4ce50433cf078b4070d6e28eb5508
iknowcando <iknowcando@yandex.ru>
2022-08-21 12:09:20 +0300

    add praxis js/my solution

--- Primes_in_numbers_5_kyu.js
+++ Primes_in_numbers_5_kyu.js
@@ -1,43 +1,42 @@
-const n = 933555431;
-
-const cb = (acc, v) => {
-  let temp = 2;
-  let power = 0;
-  while (n % Math.pow(v, temp) === 0) {
-    power = temp;
-    temp++;
-  }
-  if (power === 0) {
-    acc.push(`(${v})`);
-  } else {
-    acc.push(`(${v}**${power})`);
-  }
-  return acc;
-};
-
-//console.log(getArrayPrime(n).reduce(cb,[]).join(""))
 /*
-const isPrime=(num)=>{
-    for(let i = 2;i<num;i++){
-        if(num%i===0){
-            return false
+Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
+
+ "(p1**n1)(p2**n2)...(pk**nk)"
+with the p(i) in increasing order and n(i) empty if n(i) is 1.
+
+Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+*/
+
+export default (n)=>{
+    const cb =(acc,v)=>{
+    let temp =2;
+    let power = 0
+    while(n%Math.pow(v,temp)===0){
+        power = temp;
+        temp++;
+    }
+    if (power===0){
+        acc.push(`(${v})`)
+    }else{
+        acc.push(`(${v}**${power})`)
+    }
+    return acc
+}
+
+
+const nod =(num)=>{
+    const res = [];
+    let del = 2;
+    while(num!==1){
+        if(num%del===0){
+            res.push(del)
+            num=num/del
+        }else{
+            del++;
         }
     }
-    return true
+    return res
 }
-*/
-const nod = (num) => {
-  const res = [];
-  let del = 2;
-  while (num !== 1) {
-    if (num % del === 0) {
-      res.push(del);
-      num = num / del;
-    } else {
-      del++;
-    }
-  }
-  return res;
-};
-//console.log(Math.sqrt(933555431))
-console.log([...new Set(nod(n))].reduce(cb, []).join(""));
+
+return [...new Set(nod(n))].reduce(cb,[]).join("")
+}
