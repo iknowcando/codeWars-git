5e26f8ca9bb88470135297d1a530d7d97a5e271a
iknowcando <iknowcando@yandex.ru>
2022-08-24 15:07:36 +0300

    add praxis js/my solution

---
+++ Reverse_or_rotate_6_kyu.js
@@ -0,0 +1,62 @@
+/*
+The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+If
+
+sz is <= 0 or if str is empty return ""
+sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+Examples:
+revrot("123456987654", 6) --> "234561876549"
+revrot("123456987653", 6) --> "234561356789"
+revrot("66443875", 4) --> "44668753"
+revrot("66443875", 8) --> "64438756"
+revrot("664438769", 8) --> "67834466"
+revrot("123456779", 8) --> "23456771"
+revrot("", 8) --> ""
+revrot("123456779", 0) --> "" 
+revrot("563000655734469485", 4) --> "0365065073456944"
+Example of a string rotated to the left by one position:
+s = "123456" gives "234561".
+*/
+
+export default (str, sz) => {
+  if (sz === 0 || str === "" || sz > str.length) {
+    return "";
+  }
+  const getFindArray = (str, sz) => {
+    let start = -sz;
+    let end = 0;
+    return [...new Array(Math.ceil(str.length / sz)).keys()].map((i) =>
+      str.slice((start += sz), (end += sz))
+    );
+  };
+
+  const isReverse = (elem) => {
+    return (
+      elem
+        .split("")
+        .map(Number)
+        .map((x) => x ** 3)
+        .reduce((a, b) => a + b) %
+        2 ===
+      0
+    );
+  };
+
+  const reverse = (chnk) => {
+    return chnk.split("").reverse().join("");
+  };
+
+  const rotate = (chnk) => {
+    const workingAr = chnk.split("");
+    workingAr.push(workingAr.shift());
+
+    return workingAr.join("");
+  };
+
+  return getFindArray(str, sz)
+    .map((i) => (i.length < sz ? "" : isReverse(i) ? reverse(i) : rotate(i)))
+    .join("");
+};
